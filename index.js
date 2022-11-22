14863c8b578f203054c4e524dd52eb7ca6f44837
Grzafnan <Grzafnan>
2022-11-23 00:31:08 +0600

    done

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
+app.get('/', (req, res) => {
+  res.send('News API Running...');
+});
+
+
+app.get('/news-categories', (req, res) => {
+  res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (id === '08') {
+    res.send(news)
+    // console.log(news);
+  }
+
+  else {
+    const categoryNews = news.filter(n => n.category_id === id);
+    // console.log(categoryNews);
+    res.send(categoryNews)
+  }
+})
+
+
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+
+
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find(n => n._id === id);
+  res.send(selectedNews)
+})
+
+
+
+
+app.listen(port, () => {
+  console.log('Dragon APi listening on port 5000');
+});
